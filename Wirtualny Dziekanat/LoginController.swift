ed047b0df7100ba1b031bf9c372e9524b809da48
Jakub Nykiel <Kulker@Air-Jakub.home>
2016-05-21 11:29:27 +0200

    Dodano LoginController oraz możliwość wykasowania wpisanego tekstu

---
+++ Wirtualny Dziekanat/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginController.swift
+//  Wirtualny Dziekanat
+//
+//  Created by Jakub Nykiel on 21.05.2016.
+//  Copyright © 2016 Jakub Nykiel. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
