50c2169107593e072e99d43718aee753afc240d4
Jakub Nykiel <Kulker@pc70.home>
2016-07-03 22:34:11 +0200

    Dodano widok pionowy i kontroler dla MENU profesora

---
+++ Wirtualny Dziekanat/ProfesorMenuController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfesorMenuController.swift
+//  Wirtualny Dziekanat
+//
+//  Created by Jakub Nykiel on 03.07.2016.
+//  Copyright © 2016 Jakub Nykiel. All rights reserved.
+//
+
+import UIKit
+
+class ProfesorMenuController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
