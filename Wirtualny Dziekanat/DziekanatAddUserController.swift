ad200330fe4ecbdbdbdbd3fd75c338228f11c34c
Jakub Nykiel <j.nykiel93@gmail.com>
2016-10-03 22:13:33 +0200

    Wybór typu konta przy dodawaniu użytkownika skończony

--- Wirtualny Dziekanat/DziekanatAddUserController.swift
+++ Wirtualny Dziekanat/DziekanatAddUserController.swift
@@ -12,27 +12,14 @@ import Firebase
 class DziekanatAddUserController: UIViewController {
     
     let myFunctions = Functions()
-//    let dropDownType = DropDown()
-
-    @IBOutlet weak var dropDownTypeView: UIView!
-    
-    @IBOutlet weak var studentContainer: UIView!
-    @IBOutlet weak var profContainer: UIView!
-    @IBOutlet weak var dziekanatContainer: UIView!
-
-    @IBOutlet weak var facultyView: UIView!
-    @IBOutlet weak var semesterView: UIView!
-    @IBOutlet weak var fieldView: UIView!
-    
-    @IBOutlet weak var resultFaculty: UILabel!
-    @IBOutlet weak var resultField: UILabel!
-    @IBOutlet weak var resultSemester: UILabel!
     
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var surnameText: UITextField!
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var numbersText: UITextField!
     @IBOutlet weak var titlesText: UITextField!
+    
+    @IBOutlet weak var facultyResult: UILabel!
     
     var ref: FIRDatabaseReference!
     var refHandle: UInt!
@@ -46,28 +33,25 @@ class DziekanatAddUserController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        ref.child("faculty").observeSingleEvent(of: .value, with: { (snapshot) in
-            self.faculty = snapshot.value as! [String]
-        })
-        ref.child("semester").child("name").observeSingleEvent(of: .value, with: { (snapshot) in
-            self.semester = snapshot.value as! [String]
-        })
-        ref.child("student").child("faculty").observeSingleEvent(of: .value, with: { (snapshot) in
-            self.student = [:]
-            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
-                for snap in snapshots {
-                   let key = snap.key
-                }
-            }
-        })
+//        ref.child("faculty").observeSingleEvent(of: .value, with: { (snapshot) in
+//            self.faculty = snapshot.value as! [String]
+//        })
+//        ref.child("semester").child("name").observeSingleEvent(of: .value, with: { (snapshot) in
+//            self.semester = snapshot.value as! [String]
+//        })
+//        ref.child("student").child("faculty").observeSingleEvent(of: .value, with: { (snapshot) in
+//            self.student = [:]
+//            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
+//                for snap in snapshots {
+//                   let key = snap.key
+//                }
+//            }
+//        })
+        print(account_type)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.studentContainer?.isHidden = true
-        self.profContainer?.isHidden = true
-        self.dziekanatContainer?.isHidden = true
-    
     }
     
     override func didReceiveMemoryWarning() {
@@ -77,67 +61,8 @@ class DziekanatAddUserController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
-//        dropDownType.hide()
     }
     
-//    @IBAction func selectType(_ sender: AnyObject){
-//        
-//        dropDownType.anchorView = dropDownTypeView
-//        dropDownType.dataSource = ["Student", "Prowadzący", "Dziekanat"]
-//        dropDownType.show()
-//        
-//        dropDownType.selectionAction = { (index: Int, item: String) in
-//            sender.setTitle(item, for: .normal)
-//            self.account_type = item
-//            if(index == 0)
-//            {
-//                self.myFunctions.setAnimationView(self.studentContainer, hidden: false)
-//                self.myFunctions.setAnimationView(self.profContainer, hidden: true)
-//                self.myFunctions.setAnimationView(self.dziekanatContainer, hidden: true)
-//                
-//            }
-//            if(index == 1)
-//            {
-//                self.myFunctions.setAnimationView(self.studentContainer, hidden: true)
-//                self.myFunctions.setAnimationView(self.profContainer, hidden: false)
-//                self.myFunctions.setAnimationView(self.dziekanatContainer, hidden: true)
-//            }
-//            if(index == 2)
-//            {
-//                self.myFunctions.setAnimationView(self.studentContainer, hidden: true)
-//                self.myFunctions.setAnimationView(self.profContainer, hidden: true)
-//                self.myFunctions.setAnimationView(self.dziekanatContainer, hidden: false)
-//            }
-//        }
-//
-//    }
-//    
-//    @IBAction func selectFaculty(_ sender: AnyObject) {
-//        dropDownType.anchorView = facultyView
-//        dropDownType.direction = .bottom
-//        dropDownType.dataSource = faculty
-//        dropDownType.show()
-//        
-//        dropDownType.selectionAction = { [unowned self] (index: Int, item: String) in
-//            self.resultFaculty.adjustsFontSizeToFitWidth = true
-//            self.resultFaculty.text = item
-//        }
-//    }
-//    
-//    @IBAction func selectField(_ sender: AnyObject) {
-//        
-//    }
-//    
-//    @IBAction func selectSemester(_ sender: AnyObject) {
-//        dropDownType.anchorView = semesterView
-//        dropDownType.direction = .bottom
-//        dropDownType.dataSource = semester
-//        dropDownType.show()
-//        
-//        dropDownType.selectionAction = { [unowned self] (index: Int, item: String) in
-//            self.resultSemester.text = item
-//        }
-//    }
 //    @IBAction func addDziekanatUserButton(_ sender: AnyObject) {
 //        
 ////        let password = "s" + numbersText.text!
---
+++ Wirtualny Dziekanat/SelectAccountTypeTableViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SelectAccountTypeTableViewController.swift
+//  Wirtualny Dziekanat
+//
+//  Created by Jakub Nykiel on 03.10.2016.
+//  Copyright © 2016 Jakub Nykiel. All rights reserved.
+//
+
+import UIKit
+
+class SelectAccountTypeTableViewController: UITableViewController {
+   
+    @IBOutlet var selectType: UITableView!
+
+    var account_types = ["Student","Dziekanat","Profesor"]
+    var type = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "selectAccount")
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return account_types.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "selectAccount", for: indexPath)
+        cell.textLabel?.text = account_types[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        type = account_types[indexPath.row]
+        
+        if (indexPath.row == 0)
+        {
+            performSegue(withIdentifier: "showStudent", sender: self)
+        }
+        else if (indexPath.row == 1)
+        {
+            performSegue(withIdentifier: "showFaculty", sender: self)
+        }
+        else
+        {
+            performSegue(withIdentifier: "showProf", sender: self)
+        }
+    }
+
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "showStudent" || segue.identifier == "showFaculty" || segue.identifier == "showProf")
+        {
+            let destinationVC:DziekanatAddUserController = segue.destination as! DziekanatAddUserController
+            
+            destinationVC.account_type = type
+            
+        }
+
+        
+    }
+    
+
+}
