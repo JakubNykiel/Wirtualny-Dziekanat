ee2d557f8a908baf1b412f4699d621faf365aeb9
Jakub Nykiel <j.nykiel93@gmail.com>
2016-11-20 17:18:21 +0100

    Podstawowe wysyłanie wiadomości działa

--- Wirtualny Dziekanat/DziekanatMenuController.swift
+++ Wirtualny Dziekanat/DziekanatMenuController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 import Firebase
+import MessageUI
 
-class DziekanatMenuController: UIViewController {
+class DziekanatMenuController: UIViewController, MFMailComposeViewControllerDelegate {
     
     var field:Bool!
-
+    
     @IBOutlet weak var aboutSubjectButton: UIButton!
     @IBOutlet weak var subjectButton: UIButton!
     var ref: FIRDatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         if(subjectButton != nil)
         {
@@ -30,8 +31,9 @@ class DziekanatMenuController: UIViewController {
             aboutSubjectButton.layer.borderColor = UIColor.black.cgColor
         }
         
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -69,5 +71,34 @@ class DziekanatMenuController: UIViewController {
         }
     }
     
-
+    @IBAction func sendEmail(_ sender: Any) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["j.nykiel93@gmail.com"])
+//        mailComposerVC.setSubject("Sending you an in-app e-mail...")
+//        mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
---
+++ Wirtualny Dziekanat/MessageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MessageViewController.swift
+//  Wirtualny Dziekanat
+//
+//  Created by Jakub Nykiel on 18.11.2016.
+//  Copyright © 2016 Jakub Nykiel. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import MessageUI
+
+class MessageViewController: UIViewController,MFMailComposeViewControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
